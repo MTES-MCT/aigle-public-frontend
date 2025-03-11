03bf7700c42709ce01ec5201fbad40f57693e04f
stephenprn <stephen.perrin@2017.icam.fr>
2025-03-11 10:57:18 +0100

    init: install and test dsfr lib

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@codegouvfr/react-dsfr/main.css'
 import App from './App.tsx'
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
+
+startReactDsfr({ defaultColorScheme: "system" });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
