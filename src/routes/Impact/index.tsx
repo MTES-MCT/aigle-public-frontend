230252a43fd61c9cb97f04745c5492724d5c6df0
stephenprn <stephen.perrin@2017.icam.fr>
2025-03-14 16:03:15 +0100

    fix: https for metabase embedded

--- src/routes/Impact/index.tsx
+++ src/routes/Impact/index.tsx
@@ -8,7 +8,7 @@ const Component: React.FC = () => {
             <div className={classes.container}>
                 <h1>Découvrez l'impact positif d'Aigle</h1>
                 <iframe
-                    src="http://metabase.aigle.beta.gouv.fr/public/dashboard/b35fe033-9bb4-4f10-a0a8-bf5809383fba"
+                    src="https://metabase.aigle.beta.gouv.fr/public/dashboard/b35fe033-9bb4-4f10-a0a8-bf5809383fba"
                     frameBorder="0"
                     width="100%"
                     className={classes['embedded-metabase']}
